49a8e1ba85cd9fd28d101dc910a35973823ae1d1
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-09-26 05:25:17 +0900

    rubocop

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,26 +1,26 @@
 #
-# Cookbook Name:: openssl 
-# Recipe:: default 
+# Cookbook Name:: openssl
+# Recipe:: default
 #
 # Author:: Ryuzee <ryuzee@gmail.com>
 #
-# Copyright 2014, Ryutaro YOSHIBA 
+# Copyright 2014, Ryutaro YOSHIBA
 #
 # This software is released under the MIT License.
 # http://opensource.org/licenses/mit-license.php
 
-case node["platform"]
-when "centos", "redhat", "amazon", "scientific", "fedora"
+case node['platform']
+when 'centos', 'redhat', 'amazon', 'scientific', 'fedora'
 
-  %w{openssl openssl-devel}.each do |package_name|
+  %w(openssl openssl-devel).each do |package_name|
     package package_name do
       action :upgrade
     end
   end
 
-when "ubuntu","debian"
+when 'ubuntu', 'debian'
 
-  %w{openssl}.each do |package_name|
+  %w(openssl).each do |package_name|
     apt_package package_name do
       action :upgrade
     end
